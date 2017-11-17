9098b9e1ebe4efe4ad3f5423947be0a42daa4954
aschleg <aaron@aaronschlegel.com>
2017-11-17 06:11:24 -0800

    update appyveyor, setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='petpy',
-    version='1.0.0',
+    version='1.5.0',
     author='Aaron Schlegel',
     author_email='aaron@aaronschlegel.com',
     url='https://github.com/aschleg/petpy',
